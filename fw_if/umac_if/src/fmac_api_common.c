292192d0f7f41f50d7a1d6c49b31cd3189353b00
Ajay Parida <ajay.parida@nordicsemi.no>
2025-01-07 19:06:09 +0530

    nrf71 ipc support changes
    Signed-off-by: Ajay Parida <ajay.parida@nordicsemi.no>

--- fw_if/umac_if/src/fmac_api_common.c
+++ fw_if/umac_if/src/fmac_api_common.c
@@ -276,6 +276,7 @@ enum nrf_wifi_status nrf_wifi_fmac_fw_chunk_load(struct nrf_wifi_fmac_dev_ctx *f
 				       fw_chunk->size);
 }
 
+#ifndef CONFIG_NRF71_ON_IPC
 enum nrf_wifi_status nrf_wifi_fmac_fw_load(struct nrf_wifi_fmac_dev_ctx *fmac_dev_ctx,
 					   struct nrf_wifi_fmac_fw_info *fmac_fw)
 {
@@ -344,7 +345,7 @@ enum nrf_wifi_status nrf_wifi_fmac_fw_load(struct nrf_wifi_fmac_dev_ctx *fmac_de
 out:
 	return status;
 }
-
+#endif /* !CONFIG_NRF71_ON_IPC */
 
 struct nrf_wifi_fmac_dev_ctx *nrf_wifi_fmac_dev_add(struct nrf_wifi_fmac_priv *fpriv,
 						    void *os_dev_ctx)
@@ -517,7 +518,7 @@ enum nrf_wifi_status nrf_wifi_fmac_conf_srcoex(struct nrf_wifi_fmac_dev_ctx *fma
 	return status;
 }
 #endif /* NRF70_OFFLOADED_RAW_TX */
-
+#ifndef CONFIG_NRF71_ON_IPC
 enum nrf_wifi_status nrf_wifi_fmac_otp_mac_addr_get(struct nrf_wifi_fmac_dev_ctx *fmac_dev_ctx,
 						    unsigned char vif_idx,
 						    unsigned char *mac_addr)
@@ -582,6 +583,7 @@ enum nrf_wifi_status nrf_wifi_fmac_otp_mac_addr_get(struct nrf_wifi_fmac_dev_ctx
 out:
 	return status;
 }
+#endif /*! CONFIG_NRF71_ON_IPC */
 
 enum nrf_wifi_status nrf_wifi_fmac_rf_params_get(
 		struct nrf_wifi_fmac_dev_ctx *fmac_dev_ctx,
